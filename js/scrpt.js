c22eb1148a1d59b864161d69a2842bb8af842bed
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2023-09-12 19:35:41 -0400

    Fourth Update

---
+++ js/scrpt.js
@@ -0,0 +1,62 @@
+// Collections of all questions 
+const Questions = [{
+    q: "A Function Associated With An object is Called:",
+    a: [{ text: "Function", isCorrect: false },
+        { text: "Method", isCorrect: true },
+        { text: "Link", isCorrect: false },
+        { text: "None", isCorrect: false }
+    ]
+
+},
+
+{
+    q: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+    a: [{ text: "The User's machine running a Web browser", isCorrect: true },
+        { text: "The Web server", isCorrect: false },
+        { text: "A central machine deep within Netscape's corporate offices", isCorrect: false },
+        { text: "None of the above", isCorrect: false }
+    ]
+
+},
+
+{
+    q: "__________ JavaScript is also called server-side JavaScript",
+    a: [{ text: "Microsoft", isCorrect: false },
+        { text: "Navigator", isCorrect: false },
+        { text: "LiveWire", isCorrect: true },
+        { text: "Native", isCorrect: false }
+    ]
+
+},
+
+{
+    q: "If Button is clicked _________ Event Handler is invoked.",
+    a: [{ text: "OnSubmit()", isCorrect: false },
+        { text: "OnLoad()", isCorrect: false },
+        { text: "IsPostBack()", isCorrect: false },
+        { text: "Onclick()", isCorrect: true }
+    ]
+
+},
+
+{
+    q: "What are variables used for in JavaScript Programs?",
+    a: [{ text: "Storing numbers, dates, or other values", isCorrect: true },
+        { text: "Varying randomly", isCorrect: false },
+        { text: "Causing high-school algebra flashbacks", isCorrect: false },
+        { text: "None of the above", isCorrect: false }
+    ]
+
+},
+
+{
+    q: "Which of the following can't be done with client-side JavaScript?",
+    a: [{ text: "Validating a form", isCorrect: false },
+        { text: "Sending a form's contents by email", isCorrect: false },
+        { text: "Storing the form's contents to a database file on the server", isCorrect: true },
+        { text: "None of the above", isCorrect: false }
+    ]
+
+}
+
+]
